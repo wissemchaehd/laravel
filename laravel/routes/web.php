c078509325d06dc3532c8e4c3773dfb12d5da718
Wissem CHAHED <wissem.chahed@le-campus-numerique.fr>
2021-06-22 17:00:14 +0200

    iteration2

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', function () {
+    return view('Home page');
+});
+Route::get('/product', function () {
+    return view('Liste des produits');
+});
+
